3b2979128278cf2eedfeba143f57494a867752df
Roman <roman@Romans-MacBook-Pro.local>
2024-08-03 11:11:18 +0300

    initialize

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <div className="app">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
